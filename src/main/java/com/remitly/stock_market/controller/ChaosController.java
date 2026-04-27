282abc51a6b996942876462cb5f30f2458c9c3a2
Dzem-z <egneradam@gmail.com>
2026-04-27 15:18:07 +0200

    Bug fix: changed get to post

--- src/main/java/com/remitly/stock_market/controller/ChaosController.java
+++ src/main/java/com/remitly/stock_market/controller/ChaosController.java
@@ -1,11 +1,11 @@
 package com.remitly.stock_market.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChaosController {
-    @GetMapping("/chaos")
+    @PostMapping("/chaos")
     public void chaos() {
         Runtime.getRuntime().halt(0);
     }
